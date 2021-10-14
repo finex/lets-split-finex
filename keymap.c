a5d0db0f1c57a5649a3c499be502e451af977c96
FiNeX <287009+finex@users.noreply.github.com>
2021-10-14 23:38:41 +0200

    Fixed n/m

--- keymap.c
+++ keymap.c
@@ -289,7 +289,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_COLEMAKDHMK] = LAYOUT_ortho_4x12(
    KC_ATAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_AEQL, \
    KC_CESC, KC_GU_A, KC_AL_S, KC_CT_D, KC_F_SH, KC_G,    KC_H,    KC_J_SH, KC_CT_K, KC_AL_L, KC_GU_SC,KC_CMIN, \
-   KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_M,    KC_N,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, \
+   KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_s,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, \
    KC_GSPC, KC_HYBL, KC_MEBR, KC_SYES, KC_NBSC, KC_CDEL, KC_FUEN, KC_SYSP, KC_AGLE, KC_DOWN, KC_UP,   KC_NURI  \
 ),
 
